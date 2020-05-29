19240114c50d66fecd113277ac20a7d6a9081f4a
priyanksondagar <32874404+priyanksondagar@users.noreply.github.com>
2020-05-29 17:35:44 +0530

    Handeling query string for dynamic content

---
+++ QueryString.js
@@ -0,0 +1,11 @@
+var express=require('express')
+
+var app=express()
+app.use(express.static(__dirname+'/public'))
+app.set('view engine','ejs')
+app.set('views',__dirname+'/views')
+
+app.get('/',function(req,res){
+    res.render('index',{qs:req.query})
+});
+app.listen(9000)
